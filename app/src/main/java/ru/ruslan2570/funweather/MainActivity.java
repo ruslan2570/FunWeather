987bbed1102604c6a706e9cda7a5714859bf5aef
Ruslan Demin <dyomin.rus@new-bokino.ru>
2022-02-21 13:54:05 +0300

    Some fix

--- app/src/main/java/ru/ruslan2570/funweather/MainActivity.java
+++ app/src/main/java/ru/ruslan2570/funweather/MainActivity.java
@@ -1,16 +1,11 @@
 package ru.ruslan2570.funweather;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Process;
@@ -18,13 +13,11 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import com.google.common.io.ByteStreams;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -66,8 +59,7 @@ public class MainActivity extends AppCompatActivity {
 		locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 		if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
 				&& ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-			ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 0);
-			//startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+			startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
 		}
 
 		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000 * 5, 300, locationListener);
@@ -91,20 +83,12 @@ public class MainActivity extends AppCompatActivity {
 		});
 	}
 
-	public LocationListener locationListener = new LocationListener() {
-		@Override
-		public void onLocationChanged(@NonNull Location location) {
-		   showLocation(location);
-		}
-	};
+	public LocationListener locationListener = location -> showLocation(location);
 
-    private void showLocation(Location location) {
-        if (location == null) {
-           // throw new Exception("");
-        }
-        Log.i("Lat", Double.toString(location.getLatitude()));
-        Log.i("Long", Double.toString(location.getLongitude()));
-    }
+	private void showLocation(Location location) {
+		Log.i("Lat", Double.toString(location.getLatitude()));
+		Log.i("Long", Double.toString(location.getLongitude()));
+	}
 
 
 	class RemoteFetch extends AsyncTask<Void, Void, JSONObject> {
@@ -128,14 +112,9 @@ public class MainActivity extends AppCompatActivity {
 				//path = "http://new-bokino.ru/test.json";
 				URL url = new URL(path);
 				HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-				if (urlConnection.getResponseCode() != 200)
-					throw new IOException("Response code isn't 200");
-				else {
-					InputStream inputStream = urlConnection.getInputStream();
-					byte[] buff = ByteStreams.toByteArray(inputStream);
-					return new JSONObject(new String(buff));
-				}
-
+				InputStream inputStream = urlConnection.getInputStream();
+				byte[] buff = ByteStreams.toByteArray(inputStream);
+				return new JSONObject(new String(buff));
 			} catch (Exception e) {
 				Toast toast = Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_SHORT);
 				toast.show();
@@ -164,14 +143,13 @@ public class MainActivity extends AppCompatActivity {
 	}
 
 	public class WeatherHandler {
-		private String description;
-		private String city;
-		private double temp;
-		private double feelsLike;
-		private double windSpeed;
-		private int windDirection;
-		private int humidity;
-		private JSONObject jsonObject = null;
+		private final String description;
+		private final String city;
+		private final double temp;
+		private final double feelsLike;
+		private final double windSpeed;
+		private final int windDirection;
+		private final int humidity;
 
 		public WeatherHandler(JSONObject json) throws JSONException {
 			description = json.getJSONArray("weather").getJSONObject(0).getString("description");
@@ -195,7 +173,7 @@ public class MainActivity extends AppCompatActivity {
 			return getResources().getString(R.string.temp) + " " + temp;
 		}
 
-		public String  getFeelsLike() {
+		public String getFeelsLike() {
 			return getResources().getString(R.string.temp_feels) + " " + feelsLike;
 		}
 
@@ -205,29 +183,36 @@ public class MainActivity extends AppCompatActivity {
 
 		public String getWindDirection() {
 			String result = "";
-			if(windDirection > 348 || windDirection < 12) result = getResources().getString(R.string.N);
-			if(windDirection > 12 && windDirection < 78) result = getResources().getString(R.string.NE);
-			if(windDirection > 78 && windDirection < 101) result = getResources().getString(R.string.E);
-			if(windDirection > 101 && windDirection < 168) result = getResources().getString(R.string.SE);
-			if(windDirection > 168 && windDirection < 214) result = getResources().getString(R.string.S);
-			if(windDirection > 214 && windDirection < 259) result = getResources().getString(R.string.SW);
-			if(windDirection > 259 && windDirection < 304) result = getResources().getString(R.string.W);
-			if(windDirection > 304 && windDirection < 348) result = getResources().getString(R.string.NW);
-			return  getResources().getString(R.string.wind_direction) + " " + result;
+			if (windDirection > 348 || windDirection < 12)
+				result = getResources().getString(R.string.N);
+			if (windDirection > 12 && windDirection < 78)
+				result = getResources().getString(R.string.NE);
+			if (windDirection > 78 && windDirection < 101)
+				result = getResources().getString(R.string.E);
+			if (windDirection > 101 && windDirection < 168)
+				result = getResources().getString(R.string.SE);
+			if (windDirection > 168 && windDirection < 214)
+				result = getResources().getString(R.string.S);
+			if (windDirection > 214 && windDirection < 259)
+				result = getResources().getString(R.string.SW);
+			if (windDirection > 259 && windDirection < 304)
+				result = getResources().getString(R.string.W);
+			if (windDirection > 304 && windDirection < 348)
+				result = getResources().getString(R.string.NW);
+			return getResources().getString(R.string.wind_direction) + " " + result;
 		}
 
 		public String getHumidity() {
 			return getResources().getString(R.string.humidity) + " " + humidity + "%";
 		}
 
-		public String getFun(){
+		public String getFun() {
 			String[] bad = getResources().getStringArray(R.array.bad_weather);
 			String[] good = getResources().getStringArray(R.array.good_weather);
-			if(temp < 18) {
+			if (temp < 18) {
 				int index = (new Random()).ints(0, bad.length).iterator().nextInt();
 				return bad[index];
-			}
-			else{
+			} else {
 				int index = (new Random()).ints(0, good.length).iterator().nextInt();
 				return good[index];
 			}
